1c3cd8e191ddb07c52ca7ccda49daf8a923a5f90
Ben DeGrieck <92237169+bdegrieck@users.noreply.github.com>
2025-06-25 21:22:42 -0400

    Add initial React app

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Self Finance</h1>
+      <p>React app is running!</p>
+    </div>
+  );
+}
