fd4d478e37538e6b72c65bc9f4965ab2dcf18e4b
bdegrieck <bendegrieck@comcast.net>
2025-06-30 20:06:55 -0400

    fixes

--- app/src/logo.jsx
+++ app/src/logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logoSrc from 'SelfFinance\app\src\assests\Logo.png'
+import logoSrc from './assests/Logo.png'
 
 export default function Logo() {
-    return <img classname="logo" src={logoSrc} alt="Self Finance Logo" />;
+    return <img className="logo" src={logoSrc} alt="Self Finance Logo" />;
 }
